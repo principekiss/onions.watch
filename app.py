966cfa9cb087947b6abd17f42c38025a4186902b
tuxicorn <inesmathy0@mail.com>
2021-12-17 00:15:10 +0100

    add ping onions from db

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 from datetime import datetime
 import sqlite3
-import requests
 
 app = Flask(__name__)
 
@@ -12,17 +11,8 @@ def index():
     con = sqlite3.connect('onions.db')
     con.row_factory = sqlite3.Row
     db = con.cursor()
-    getonions = db.execute('select name, address, status, status_nb from Onions')
+    getonions = db.execute('select * from Onions')
     return render_template('index.html', last_update=last_update, onions=getonions.fetchall())
-
-#onions = db.execute('select address from Onions')
-#    proxies = {
-#        'http': 'socks5h://127.0.0.1:9050',
-#        'https': 'socks5h://127.0.0.1:9050'
-#    }
-#    for onion in onions:
-#        data = requests.get("{{ onion['address'] }}",proxies=proxies)
-#        print(data)
 
 
 
--- pysocks.py
+++ pysocks.py
@@ -1,10 +1,17 @@
 import requests
+import sqlite3
+
+con = sqlite3.connect('onions.db')
+con.row_factory = sqlite3.Row
+db = con.cursor()
+onions = db.execute('select address from Onions')
 
 proxies = {
     'http': 'socks5h://127.0.0.1:9050',
     'https': 'socks5h://127.0.0.1:9050'
 }
-{% for onion in onions %}
-  data = requests.get("{{ onion['address'] }}",proxies=proxies)
-  print(data)
-{% endfor %}
+
+rows = onions.fetchall()
+for row in rows:
+  ping = requests.get(url=row[0],proxies=proxies)
+  print(ping.status_code)
