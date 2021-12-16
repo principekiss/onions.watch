61a92162e454cdf207cf86fb008be2d8632ff5cc
tuxicorn <inesmathy0@mail.com>
2021-12-16 12:52:45 +0100

    replace onion.png

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from datetime import datetime
 import sqlite3
+import requests
 
 app = Flask(__name__)
 
@@ -13,6 +14,16 @@ def index():
     db = con.cursor()
     getonions = db.execute('select name, address, status, status_nb from Onions')
     return render_template('index.html', last_update=last_update, onions=getonions.fetchall())
+
+#onions = db.execute('select address from Onions')
+#    proxies = {
+#        'http': 'socks5h://127.0.0.1:9050',
+#        'https': 'socks5h://127.0.0.1:9050'
+#    }
+#    for onion in onions:
+#        data = requests.get("{{ onion['address'] }}",proxies=proxies)
+#        print(data)
+
 
 
 
--- pyenv/bin/activate.fish
+++ pyenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/tuxicorn/dd-flask/pyenv"
+set -gx VIRTUAL_ENV "/home/tuxicorn/cyberiacafe/pyenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
