5015c194a6383d54f87c08f3801636a7dcec4288
tuxicorn <inesmathy0@mail.com>
2021-12-18 02:12:21 +0100

    update status for each address in DB with requests and exceptions

--- pysocks.py
+++ pysocks.py
@@ -4,20 +4,32 @@ import sqlite3
 con = sqlite3.connect('onions.db')
 con.row_factory = sqlite3.Row
 db = con.cursor()
-onions = db.execute('SELECT address FROM Onions')
+onion_address = db.execute('SELECT address FROM Onions')
 
 proxies = {
     'http': 'socks5h://127.0.0.1:9050',
     'https': 'socks5h://127.0.0.1:9050'
 }
 
-rows = onions.fetchall()
+rows = onion_address.fetchall()
+
 for row in rows:
-  ping = requests.get(url=row[0],proxies=proxies)
-  res = ping.status_code
-  print(res)
-  if res == 200:
-    query="UPDATE Onions SET status=?,status_nb=?"
-    values=(res,'status1')
-    db.execute(query,values)
-    con.commit()
+  res = None
+  try:
+    ping = requests.get(url=row[0],proxies=proxies, allow_redirects=True)
+    res = ping.status_code
+    if res == 200:
+      query="UPDATE Onions SET status=?,status_nb=? WHERE address=?"
+      values=('online','status1', row[0])
+      db.execute(query,values)
+      con.commit()
+    else: ping.raise_for_status()
+    continue
+  except:
+      query="UPDATE Onions SET status=?,status_nb=? WHERE address=?"
+      values=('offline','status0', row[0])
+      db.execute(query,values)
+      con.commit() 
+  finally:
+    print(res)
+
