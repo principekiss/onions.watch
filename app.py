fc070bc99cddb1687efe41a0967392c84c58dd93
Altitu <alan.turpin51@gmail.com>
2022-01-02 13:28:24 +0100

    update_db() is now called every 15sec, with debug code

--- app.py
+++ app.py
@@ -1,22 +1,47 @@
+#! /bin/env python3
+# -*- coding: utf-8 -*-
+
 from flask import Flask, render_template
 from datetime import datetime
 import sqlite3
+import threading
+import time
+import pysocks
 
 app = Flask(__name__)
 
-last_update = datetime.utcnow().strftime("%a, %d %b %Y, %H:%M:%S")
+STARTED = False
+
+
+def last_update():
+  return datetime.utcnow().strftime("%a, %d %b %Y, %H:%M:%S")
+
+def run_pysocks():
+  while True:
+    print("\nRunning")
+    print(threading.enumerate())
+    pysocks.update_db()
+    time.sleep(15) #*60 FIXME
 
 @app.route('/', methods=['GET'])
 def index():
-    con = sqlite3.connect('onions.db')
-    con.row_factory = sqlite3.Row
-    db = con.cursor()
-    getonions = db.execute('select * from Onions')
-    return render_template('index.html', last_update=last_update, onions=getonions.fetchall())
+  global STARTED
+  
+  if not STARTED:
+    thread_pysocks = threading.Thread(target = run_pysocks)
+    thread_pysocks.setDaemon(True)
+    thread_pysocks.start()
+    STARTED = True
+
+  con = sqlite3.connect('onions.db')
+  con.row_factory = sqlite3.Row
+  db = con.cursor()
+  getonions = db.execute('select * from Onions')
+  return render_template('index.html', last_update=last_update(), onions=getonions.fetchall())
 
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+  app.run(debug=True,host='0.0.0.0')
 
--- pysocks.py
+++ pysocks.py
@@ -1,41 +1,48 @@
 import requests
 import sqlite3
 
-con = sqlite3.connect('onions.db')
-con.row_factory = sqlite3.Row
-db = con.cursor()
-onion_address = db.execute('SELECT address FROM Onions')
 
 proxies = {
     'http': 'socks5h://127.0.0.1:9050',
     'https': 'socks5h://127.0.0.1:9050'
 }
 
-rows = onion_address.fetchall()
+def update_db():
 
-for row in rows:
-  res = "Offline"
-  try:
-    ping = requests.get(url=row[0],proxies=proxies)
-    res = ping.status_code
-    if res == 200:
-      query="UPDATE Onions SET status=?,status_nb=? WHERE address=?"
-      values=('online','status1', row[0])
-      db.execute(query,values)
-      con.commit()
-    else: 
-      ping.raise_for_status()
-    continue
-  except:
-      query="UPDATE Onions SET status=?,status_nb=? WHERE address=?"
-      values=('offline','status0', row[0])
-      db.execute(query,values)
-      con.commit() 
-  finally:
-    if ping.history:
-        for redirects in ping.history:
-          print(redirects.status_code, redirects.url, "➜", ping.status_code, ping.url)
-    else:
+  con = sqlite3.connect('onions.db')
+  con.row_factory = sqlite3.Row
+  db = con.cursor()
+  onion_address = db.execute('SELECT address FROM Onions')
+  rows = onion_address.fetchall()
+
+  for row in rows:
+    res = "Offline"
+    try:
+      ping = requests.get(url=row[0],proxies=proxies)
+      res = ping.status_code
+      if res == 200:
+        query="UPDATE Onions SET status=?,status_nb=? WHERE address=?"
+        values=('online','status1', row[0])
+        db.execute(query,values)
+        con.commit()
+      else: 
+        ping.raise_for_status()
+      continue
+    except:
+        query="UPDATE Onions SET status=?,status_nb=? WHERE address=?"
+        values=('offline','status0', row[0])
+        db.execute(query,values)
+        con.commit() 
+    finally:
+      try:
+        if ping.history:
+            for redirects in ping.history:
+              print(redirects.status_code, redirects.url, "➜", ping.status_code, ping.url)
+        else:
+            print(res)
+      except:
+        pass
+      finally:
         print(res)
 
 
